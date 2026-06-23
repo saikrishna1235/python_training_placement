26e0eec3070b8cb27a612de81692ac533b99d9bd
saikrishna1235 <saikrishnakoyyada20669@gmail.com>
2026-06-23 14:33:00 +0530

    Add polymorphism examples with class method overrides

---
+++ polymorphism/polymoorphism.py
@@ -0,0 +1,12 @@
+class A:
+    def a(self):
+        print("Welcome")
+class B:
+    def a(self):
+        print("Good bye")
+class C:
+    def a(self):
+        print("Get lost")
+
+obj=C()
+obj.a()
