50ad96735a7dafaa847653b07242c58f62ae7996
saikrishna1235 <saikrishnakoyyada20669@gmail.com>
2026-06-25 12:10:43 +0530

    Add Stack and Valid Parenthesis implementations

---
+++ stack.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self,data):
+        if len(self.stack) <5:
+            self.stack.append(data)
+        else:
+            print("OverFlow")
+            return
+    def pop(self):
+        if len(self.stack) == 0:
+            print("Stack underflow")
+            return
+        else:
+            self.stack.pop()
+    def isEmpty(self):
+        return len(self.stack) == 0
+    def peek(self):
+        print(self.stack[-1])
+    def display(self):
+        print(self.stack)
+s = Stack()
+s.push(5)
+s.push(10)
+s.push(15)
+s.push(20)
+s.push(25)
+# s.push(30)
---
+++ valid_paranthesis.py
@@ -0,0 +1,13 @@
+def Valid_p(s):
+    stack = []
+    for ch in s:
+        if ch in "({[":
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            top=stack.pop()
+            if (ch==")" and top != "(") or  (ch=="{" and top != "{" ) or (ch=="[" and top != "]"):
+                return False
+    return len(stack) == 0
+print(Valid_p("){[]})"))
