208ffabd4f7264dd4f1012df7ccf247cec499eed
saikrishna1235 <saikrishnakoyyada20669@gmail.com>
2026-06-26 10:52:44 +0530

    Refactor binary search to ternary search implementation with updated logic and example usage

--- ternary_search.py
+++ ternary_search.py
@@ -1,20 +1,21 @@
-def binary_search(ls,target):
-    low = 0
-    high = len(ls)-1
-    while low <= high:
-        mid1 = low+(high-low)//3
-        mid2 = high-(high-low)//3
-        if ls[mid1] == target:
+def ternary_search(ls,target):
+    low=0
+    high=len(ls)-1
+    while low<=high:
+        mid1=low+(high+low)//3
+        mid2=high-(high+low)//3
+        if ls[mid1]==target:
             return mid1
-        elif ls[mid2] == target:
+        if ls[mid2]==target:
             return mid2
-        elif ls[mid1] > target :
-            high = mid1 - 1
-        elif ls[mid2] < target :
-            low = mid2 + 1
-        else:
+        if(ls[mid1]<target and ls[mid2]>target):
             low=mid1+1
             high=mid2-1
+        elif ls[mid1]>target:
+            low=0
+            high=mid1-1
+        else:
+            low=mid2+1
     return -1
-ls = [1,2,3,4,5,6,7,8,9]
-print(binary_search(ls,8))  
+ls=[0,1,3,5,7,9,11,13,15]
+print(ternary_search(ls,13))
